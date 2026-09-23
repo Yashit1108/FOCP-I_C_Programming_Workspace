92525e70c006e1a6050e65e948f9113346cdf6ad
Yashit Soni <yashitsoni08@gmail.com>
2026-09-23 20:10:46 +0530

    Added Assignment 1

---
+++ assignment1.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+
+int main() {
+    //question 1
+//     int studentID = 101;
+//     int age = 18;
+//     float percentage = 93.5;
+//     char grade = 'A';
+
+//     printf("Student ID: %d\n", studentID);
+//     printf("Age: %d\n", age);
+//     printf("Percentage: %.2f%%\n", percentage);
+//     printf("Grade: %c\n", grade);
+
+//     return 0;
+// }
+    //question 2
+//     int age;
+//     float height;
+//     char grade;
+
+//     printf("Enter your age: ");
+//     scanf("%d", &age);
+
+//     printf("Enter your height in meters: ");
+//     scanf("%f", &height);
+
+//     printf("Enter your grade: ");
+//     scanf(" %c", &grade);
+
+//     printf("\nAge: %d\n", age);
+//     printf("Height: %.2f meters\n", height);
+//     printf("Grade: %c\n", grade);
+
+//     return 0;
+// }
+    //question 3
+//     float price, totalBill;
+//     int quantity;
+
+//     printf("Enter price of one item: ");
+//     scanf("%f", &price);
+
+//     printf("Enter quantity purchased: ");
+//     scanf("%d", &quantity);
+//     totalBill = price * quantity;
+
+//     printf("Total Bill: %.2f\n", totalBill);
+
+//     return 0;
+// }
+    //question 4
+//     int a, b, c;
+//         float average;
+
+//         printf("Enter first number: ");
+//         scanf("%d", &a);
+
+//         printf("\nEnter second number: ");
+//         scanf("%d", &b);
+
+//         printf("\nEnter third number: ");
+//         scanf("%d", &c);
+
+//         average = (a + b + c) / 3.0;
+
+//         printf("\nAverage = %.2f\n", average);
+
+//         return 0;
+// }
+    //question 5 
+//     int a, b, temp;
+
+//     printf("Enter first number: ");
+//     scanf("%d", &a);
+
+//     printf("\nEnter second number: ");
+//     scanf("%d", &b);
+
+//     printf("\nBefore swapping: %d %d\n", a, b);
+
+//     temp = a;
+//     a = b;
+//     b = temp;
+
+//     printf("After swapping: %d %d\n", a, b);
+
+//     return 0;
+// }
+    //question 6
+//     int a, b, quotient, remainder;
+
+//     printf("Enter first number: ");
+//     scanf("%d", &a);
+
+//     printf("\nEnter second number: ");
+//     scanf("%d", &b);
+
+//     quotient = a / b;
+//     remainder = a % b;
+
+//     printf("\nQuotient = %d\n", quotient);
+//     printf("Remainder = %d\n", remainder);
+
+//     return 0;
+// }
+    //question 7
+//     int m1, m2, m3, m4, m5, total;
+//     float percentage;
+
+//     printf("Enter marks of subject 1: ");
+//     scanf("%d", &m1);
+
+//     printf("\nEnter marks of subject 2: ");
+//     scanf("%d", &m2);
+
+//     printf("\nEnter marks of subject 3: ");
+//     scanf("%d", &m3);
+
+//     printf("\nEnter marks of subject 4: ");
+//     scanf("%d", &m4);
+
+//     printf("\nEnter marks of subject 5: ");
+//     scanf("%d", &m5);
+
+//     total = m1 + m2 + m3 + m4 + m5;
+
+//     percentage = total / 5.0;
+
+//     printf("\nTotal Marks = %d\n", total);
+//     printf("Percentage = %.2f%%\n", percentage);
+
+//     return 0;
+// }
+    //question 8
+//     float basicSalary, allowance, bonus, finalSalary;
+
+//     printf("Enter Basic Salary: ");
+//     scanf("%f", &basicSalary);
+
+//     printf("\nEnter Allowance: ");
+//     scanf("%f", &allowance);
+
+//     printf("\nEnter Bonus: ");
+//     scanf("%f", &bonus);
+
+//     finalSalary = basicSalary + allowance + bonus;
+
+//     printf("\nFinal Salary = %.2f\n", finalSalary);
+
+//     return 0;
+// }
+    //question 9
+//     float celsius, fahrenheit;
+
+//     printf("Enter temperature in Celsius: ");
+//     scanf("%f", &celsius);
+
+//     fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+//     printf("\nTemperature in Fahrenheit = %.2f F\n", fahrenheit);
+
+//     return 0;
+// }
+    //question 10
+//     int productID, quantity;
+//     float price, discountPercentage, subtotal, discountAmount, finalAmount;
+
+//     printf("Enter Product ID: ");
+//     scanf("%d", &productID);
+
+//     printf("\nEnter Product Price: ");
+//     scanf("%f", &price);
+
+//     printf("\nEnter Quantity: ");
+//     scanf("%d", &quantity);
+
+//     printf("\nEnter Discount Percentage: ");
+//     scanf("%f", &discountPercentage);
+
+//     subtotal = price * quantity;
+
+//     discountAmount = subtotal * discountPercentage / 100;
+
+//     finalAmount = subtotal - discountAmount;
+
+//     printf("\nProduct ID = %d\n", productID);
+//     printf("Subtotal = %.2f\n", subtotal);
+//     printf("Discount Amount = %.2f\n", discountAmount);
+//     printf("Final Payable Amount = %.2f\n", finalAmount);
+
+//     return 0;
+// }
